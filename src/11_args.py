16d2c168120c415cb13c5ab808cbc07a623bba04
Khalil <khalilfoulks@gmail.com>
2020-09-01 17:48:51 -0400

    finished problem 11

--- src/11_args.py
+++ src/11_args.py
@@ -28,8 +28,8 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
 
-# How do you have to modify the f2 call below to make this work? 
-print(f2(a))    # Should print 22
+# How do you have to modify the f2 call below to make this work?
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -56,6 +56,10 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+value = 0
+def f4(**arg):
+    for key in arg:
+        print(f"key: {key}, value: {arg[key]}")
 
 # Should print
 # key: a, value: 12
@@ -74,4 +78,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
